b493f5f1465bc25e62d142c9a94884ac9f7bddc0
talha-irshad <tirshad94@gmail.com>
2020-06-05 15:30:36 +0500

    first commit

---
+++ Chap 1/app.js
@@ -0,0 +1,21 @@
+// task 1
+alert("Welcome to my Website");
+
+// task 2
+alert("Error! Please enter a valid password.")
+
+// task 3
+alert("Welcome to JS land....\nHappy coding!")
+
+// task 4
+alert("Welcome to JS land...")
+alert("Happy Coding!")
+
+// task 5
+alert("Check chapter 1 folder for Screenshot for task 5")
+
+// task 6
+alert("Check chapter 1 folder for Screenshot for task 6")
+
+// task 7
+alert("Changing the placement of <Script> has no effect on my project")
